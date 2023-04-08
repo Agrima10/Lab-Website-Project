7a1a941c9d999a12fb0b29acc6a75a694c2160cd
sana presswala <105861345+sana-presswala@users.noreply.github.com>
2023-04-09 03:12:32 +0530

    Update admin.py

--- labwebsite/api/admin.py
+++ labwebsite/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Publication
+from .models import Project
 
+admin.site.register(Publication)
+admin.site.register(Project)
 # Register your models here.
+
