93ab32038a1da69da0a32ce49980a04d735cb6c7
sana presswala <105861345+sana-presswala@users.noreply.github.com>
2023-04-11 18:19:56 +0530

    Update views.py

--- labwebsite/api/views.py
+++ labwebsite/api/views.py
@@ -11,7 +11,28 @@ from rest_framework import generics
 from rest_framework import mixins
 from rest_framework import viewsets
 
+
 class LabMembersViewSet(viewsets.ModelViewSet):
     queryset=LabMember.objects.all()
     serializer_class=LabMemberSerializer
+
+
+class ProfessorViewSet(viewsets.ModelViewSet):
+    queryset=Professor.objects.all()
+    serializer_class=ProfessorSerializer
+
+
+class AwardViewSet(viewsets.ModelViewSet):
+    queryset=Award.objects.all()
+    serializer_class=AwardSerializer
+
+
+class PublicationViewSet(viewsets.ModelViewSet):
+    queryset=Publication.objects.all()
+    serializer_class=PublicationSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset=Project.objects.all()
+    serializer_class=ProjectSerializer
 
