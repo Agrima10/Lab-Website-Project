b59d0eaaea1dbddafbe2b9848d8c23ebfc2bcc79
sana presswala <105861345+sana-presswala@users.noreply.github.com>
2023-04-11 16:41:35 +0530

    Update views.py

--- labwebsite/api/views.py
+++ labwebsite/api/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import LabMember
+from .serializers import LabMemberSerializer
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
 # Create your views here.
-def Index(request):
-    return HttpResponse("it is working")
+
+
+def LabMember_list(request):
+    if request.method == 'GET':
+        LabMember=LabMember.objects.all()
+        serializer=LabMemberSerializer(LabMember,many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method=='POST':
+        data=JSONParser().parse(request)
+        serializer=LabMemberSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data,status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
