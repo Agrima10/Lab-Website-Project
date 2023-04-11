ff8ff5b9bad087cbd10b15454ae0aecef065405e
Agrima10 <agrimabundela10@gmail.com>
2023-04-11 18:27:08 +0530

    added urls

--- labwebsite/api/urls.py
+++ labwebsite/api/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
-from .views import LabMembersViewSet
+from .views import LabMembersViewSet, ProfessorViewSet, AwardViewSet, PublicationViewSet, ProjectViewSet
 from rest_framework.routers import DefaultRouter
 # from .views import Lab_Member
 # from .views import Professor_
 # from .views import LabMember_list,ProfessorMember_list,Award_list,Publication_list,Project_list
 router= DefaultRouter()
 router.register('members',LabMembersViewSet, basename='members')
+router.register('professor',ProfessorViewSet, basename='professor')
+router.register('awards',AwardViewSet, basename='awards')
+router.register('publications',PublicationViewSet, basename='publications')
+router.register('projects',ProjectViewSet, basename='projects')
 urlpatterns = [
     path('',include(router.urls)),
 ]
