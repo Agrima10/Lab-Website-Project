b1883a09b0f2ed87ebf3f8733103cb9b99e0fdfc
sana presswala <105861345+sana-presswala@users.noreply.github.com>
2023-04-11 17:25:18 +0530

    Update views.py

--- labwebsite/api/views.py
+++ labwebsite/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
-from .models import LabMember
-from .serializers import LabMemberSerializer
+from .models import LabMember, Professor, Award, Publication, Project
+from .serializers import LabMemberSerializer, ProfessorSerializer, AwardSerializer, PublicationSerializer, ProjectSerializer
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 # Create your views here.
@@ -8,8 +8,8 @@ from rest_framework.parsers import JSONParser
 
 def LabMember_list(request):
     if request.method == 'GET':
-        LabMember=LabMember.objects.all()
-        serializer=LabMemberSerializer(LabMember,many=True)
+        Lab_Member=LabMember.objects.all()
+        serializer=LabMemberSerializer(Lab_Member,many=True)
         return JsonResponse(serializer.data, safe=False)
     
     elif request.method=='POST':
@@ -19,5 +19,60 @@ def LabMember_list(request):
             serializer.save()
             return JsonResponse(serializer.data,status=201)
         return JsonResponse(serializer.errors, status=400)
-
-
+    
+def ProfessorMember_list(request):
+    if request.method == 'GET':
+        Professor_=Professor.objects.all()
+        serializer=ProfessorSerializer(Professor_,many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method=='POST':
+        data=JSONParser().parse(request)
+        serializer=ProfessorSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data,status=201)
+        return JsonResponse(serializer.errors, status=400)
+    
+def Award_list(request):
+    if request.method == 'GET':
+        Award_=Award.objects.all()
+        serializer=AwardSerializer(Award_,many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method=='POST':
+        data=JSONParser().parse(request)
+        serializer=AwardSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data,status=201)
+        return JsonResponse(serializer.errors, status=400)
+    
+def Publication_list(request):
+    if request.method == 'GET':
+        Publication_=Publication.objects.all()
+        serializer=PublicationSerializer(Publication_,many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method=='POST':
+        data=JSONParser().parse(request)
+        serializer=PublicationSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data,status=201)
+        return JsonResponse(serializer.errors, status=400)
+    
+def Project_list(request):
+    if request.method == 'GET':
+        project_=Project.objects.all()
+        serializer=ProjectSerializer(project_,many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method=='POST':
+        data=JSONParser().parse(request)
+        serializer=ProjectSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data,status=201)
+        return JsonResponse(serializer.errors, status=400)
+    
