bd95209879c80223c72cc60f5ee219dae0b279ab
sana presswala <105861345+sana-presswala@users.noreply.github.com>
2023-04-11 17:34:23 +0530

    Update urls.py

--- labwebsite/api/urls.py
+++ labwebsite/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 # from .views import Index 
-from .views import LabMember_list
+from .views import LabMember_list,ProfessorMember_list,Award_list,Publication_list,Project_list
 
 urlpatterns = [
     # path('', Index),
     path('members/',LabMember_list),
+    path('members/',ProfessorMember_list),
+    path('members/',Award_list),
+    path('members/',Publication_list),
+    path('members/',Project_list),
 ]
