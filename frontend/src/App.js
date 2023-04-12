3683322ea96e21abdd9c91de6558de7ba65e7d5f
Agrima10 <agrimabundela10@gmail.com>
2023-04-12 16:01:40 +0530

    added navbar and other routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import Homepage from './components/Homepage.js';
-import { useState } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom'
-
+import Awards from './components/Awards';
+import Navbar1 from './components/Navbar';
+import Footer from './components/Footer';
+import News from './components/News';
+import People from './components/People';
+import Patents from './components/Patents';
+import Research from './components/Research';
+// import People from './components/People';
+// import { useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar1/>
        <Routes>
           <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/awards" element={<Awards />} />
+          <Route exact path="/news" element={<News />} />
+          <Route exact path="/patents" element={<Patents/>} />
+          <Route exact path="/people" element={<People />} />
+          <Route exact path="/research" element={<Research />} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
