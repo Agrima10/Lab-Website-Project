28bc612b259f8aa04baa2d6fa9bd98d3d4cdce3d
sana presswala <105861345+sana-presswala@users.noreply.github.com>
2023-04-12 16:40:21 +0530

    abc

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,14 +7,16 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 function Navbar1() {
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="lg" style={{ backgroundColor: "#04395E" }} variant="dark">
       <Container>
-        <Navbar.Brand href="#home">LAB</Navbar.Brand>
+        <Navbar.Brand href="#home"><img src="./src/image/Indian_Institute_of_Technology,_Indore_Logo.png" alt="example" width="200" height="150"/>Dr. Puneet Gupta</Navbar.Brand>
+        {/* <Navbar.Brand href="#home">Dr. Puneet Gupta</Navbar.Brand> */}
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
+            {/* <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
+
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -25,13 +27,29 @@ function Navbar1() {
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
+            <Nav.Link href="#deets">Home</Nav.Link>
+            <Nav.Link href="#People">People</Nav.Link>
+            <Nav.Link href="#r">Research</Nav.Link>
+            <Nav.Link href="#pp">Publications and Patents</Nav.Link>
+            <Nav.Link href="#a">Awards and Fellowships</Nav.Link>
+            <NavDropdown title="More" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            {/* <Nav.Link eventKey={2} href="#memes">
               Dank memes
-            </Nav.Link>
+            </Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
