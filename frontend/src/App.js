7e79aa49148c68e834e551db3831c3f7a5f7ff10
Agrima10 <agrimabundela10@gmail.com>
2023-04-12 14:55:58 +0530

    made comp folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import homepage from './components/homepage';
 
 function App() {
   return (
     <div className="App">
       frontend goes here
+      <homepage/>
     </div>
   );
 }
