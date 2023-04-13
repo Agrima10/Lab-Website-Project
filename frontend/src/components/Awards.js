543825402c46aeed724dda2d41f0179155b89b16
Agrima10 <agrimabundela10@gmail.com>
2023-04-14 01:12:00 +0530

    added data get code, logo

--- frontend/src/components/Awards.js
+++ frontend/src/components/Awards.js
@@ -1,6 +1,18 @@
 import React from 'react'
-
+import axios from 'axios'
+import { useState,useEffect } from 'react'
 function Awards() {
+
+  const [data,setData]= useState();
+  const [isError,setIsError]=useState();
+  useEffect(()=>{
+    axios.get('http://127.0.0.1:8000/api/awards/').then((response) => setData(response.data))
+    .catch((error) => setIsError(error.message));
+  if (!isError) {
+    setData("Not Available");
+  }
+  },[data])
+  console.log(data);
   return (
     <div>awards</div>
   )
