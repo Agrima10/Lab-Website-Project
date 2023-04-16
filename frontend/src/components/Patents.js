9c0780b26388c04ba7847d4b75b0b8587e212a11
Agrima10 <agrimabundela10@gmail.com>
2023-04-16 17:04:06 +0530

    data to frontend

--- frontend/src/components/Patents.js
+++ frontend/src/components/Patents.js
@@ -1,8 +1,34 @@
 import React from 'react'
-
+import axios from 'axios'
+import { useState,useEffect } from 'react'
 function Patents() {
+  const [data,setData]= useState();
+  const [isError,setIsError]=useState();
+  useEffect(()=>{
+    axios.get('http://127.0.0.1:8000/api/publications/',
+    {headers: {
+      'Content-Type':'application/json',
+      'Authorization':'Token 7c22c44ef8744aa74d9fbb8bf3c8ad8d6b32f291'
+    }}).then((response) => setData(response.data))
+    .catch((error) => setIsError(error.message));
+  if (isError) {
+    setData("Not Available");
+  }
+  },[])
+  console.log(data);
   return (
-    <div>Patents</div>
+    <div>
+      {
+        data?.map((item,key)=>(
+          <>
+          <div>
+            {item.title}
+            {/* {item.AwardName} */}
+          </div>
+          </>
+        ))
+      }
+    </div>
   )
 }
 
