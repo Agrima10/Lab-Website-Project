23cdf12e90245e8f2e1ff1c58d48c1c2f03755e4
sana presswala <105861345+sana-presswala@users.noreply.github.com>
2023-04-23 13:03:25 +0530

    jndd

--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -25,7 +25,8 @@ function Awards() {
     axios.get('http://127.0.0.1:8000/api/news/',
     {headers: {
       'Content-Type':'application/json',
-      'Authorization':'Token 672875923a6a356c94a3d5db720e85af9f4aca79'
+      // 'Authorization':'Token 672875923a6a356c94a3d5db720e85af9f4aca79'
+      'Authorization': 'Token 311267cd55dd503028063abcf2ca1c96ad877fc7',
       //672875923a6a356c94a3d5db720e85af9f4aca79 
     }}).then((response) => setData(response.data))
     .catch((error) => setIsError(error.message));
